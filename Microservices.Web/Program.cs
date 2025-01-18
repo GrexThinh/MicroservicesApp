e3c7cc04e963ee6ca42e90d14e40fa542fe9981b
Grex Thinh <vanthinhnq@gmail.com>
2025-01-18 11:51:29 +0700

    add product function

--- Microservices.Web/Program.cs
+++ Microservices.Web/Program.cs
@@ -11,13 +11,16 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
 builder.Services.AddHttpClient<ITokenProvider, TokenProvider>();
 builder.Services.AddHttpClient<ICouponService, CouponService>();
+builder.Services.AddHttpClient<IProductService, ProductService>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 
+SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
 SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
---
+++ Microservices.Web/Service/IService/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Microservices.Web.Models;
+using MicroservicesApp.Web.Models;
+
+namespace Microservices.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> GetProductAsync(string productCode);
+        Task<ResponseDto?> GetAllProductsAsync();
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+        Task<ResponseDto?> CreateProductAsync(ProductDto productDto);
+        Task<ResponseDto?> UpdateProductAsync(ProductDto productDto);
+        Task<ResponseDto?> DeleteProductAsync(int id);
+    }
+}
